2e69e31855a2bac85d89fd3fce414bd160d89c26
Reese Dame <reesedame@gmail.com>
2023-04-24 15:11:35 -0700

    Add controller & routes for stacks

--- controllers/stacks.js
+++ controllers/stacks.js
@@ -1,1 +1,50 @@
-const Stack = require('../models/stack');
+const Stack = require("../models/stack");
+
+function newStack(req, res) {
+	res.render("stacks/new", { title: "Enter a New Movie" });
+}
+
+async function create(req, res) {
+	try {
+		// delete empty fields from req.body
+		for (let key in req.body) {
+			if (req.body[key] === "") delete req.body[key];
+		}
+
+		await Stack.create(req.body);
+		res.redirect("/stacks");
+	} catch (error) {
+		console.log(error);
+		res.render("error", { title: "Something Went Wrong" });
+	}
+}
+
+async function index(req, res) {
+	try {
+		const stacks = await Stack.find();
+		res.render("index", { stacks: stacks, title: "All Stacks" });
+	} catch (error) {
+		console.log(error);
+		res.render("error", { title: "Something Went Wrong" });
+	}
+}
+
+async function show(req, res) {
+	try {
+		const foundStack = await Stack.findById(req.params.id);
+		res.render("stacks/show", {
+			stacks: foundStack,
+			title: "See Stack Details",
+		});
+	} catch (error) {
+		console.log(error);
+		res.render("error", { title: "Something Went Wrong" });
+	}
+}
+
+module.exports = {
+	new: newStack,
+	create,
+	index,
+	show,
+};
--- routes/stacks.js
+++ routes/stacks.js
@@ -2,4 +2,16 @@ const express = require("express");
 const router = express.Router();
 const stacksController = require("../controllers/stacks");
 
+// GET /stacks - Index Route
+router.get("/", stacksController.index);
+
+// GET /stacks/new - New Stack Route
+router.get("/new", stacksController.new);
+
+// POST /stacks - Create Route
+router.post("/", stacksController.create);
+
+// GET /stacks/:id - Show Route
+router.get("/:id", stacksController.show);
+
 module.exports = router;
