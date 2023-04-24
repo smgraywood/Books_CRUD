23277f7a1d864497679ae194dd09a70368b16bf5
dolveda <dolveda0@gmail.com>
2023-04-24 15:25:44 -0700

    create controllers books.js

---
+++ controllers/books.js
@@ -0,0 +1,63 @@
+const Book = require('../models/stack')
+
+function newBook(req,res) {
+    res.render('books/new', {
+        title: 'Enter a New Book'
+    });
+};
+
+async function create(req,res) {
+    try{
+        for(let key in req.body) {
+            if(req.body[key] === '') delete req.body[key];
+        };
+
+        if(req.body.author) {
+            req.body.author = req.body.cast.trim();
+            req.body.author = req.body.author.split(/\s*,\s*/);
+        }
+
+        await Book.create(req.body);
+
+        res.redirect('/books');
+
+    }  catch (error) {
+        console.log(error);
+        res.render('error', {title: 'Something Went Wrong'});
+    }
+};
+
+async function index(req,res) {
+    try{
+        const allBookss = await Book.find({})
+
+        res.render('books/index', {
+            books: allBookss,
+            title: 'All Books'
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.render('error', {title: 'Something Went Wrong'});
+    }
+};
+
+async function show(req, res) {
+    try {
+        const foundBook = await Book.findById(req.params.id);
+        res.render('books/show', {
+            movie: foundBook,
+            title: 'See Book Details'
+        });
+    } catch (error) {
+        console.log(error);
+        res.render('error', {title: 'Something Went Wrong'});
+    }
+ }
+
+ module.exports = {
+    new: newBook,
+    create,
+    index,
+    show
+ }
