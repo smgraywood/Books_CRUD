4d917a75b0b85736c022599b8722f590d1cd0582
Reese Dame <reesedame@gmail.com>
2023-04-24 10:00:14 -0700

    Add user model & serialization

--- server.js
+++ server.js
@@ -1,1 +1,41 @@
-DATABASE_URL="mongodb+srv://smgraywood:QbCPHk1cOSnCSIPr@cluster0.qztwt78.mongodb.net/stacks?retryWrites=true&w=majority"
+// require dependencies
+const express = require("express");
+const logger = require("morgan");
+const session = require("express-session");
+const passport = require("passport");
+
+// initialize app
+const app = express();
+
+// configure app settings
+app.set("view engine", "ejs");
+require("dotenv").config();
+require("./config/database");
+require("./config/passport");
+
+// mount middleware
+app.use(logger("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(
+	session({
+		secret: process.env.SECRET,
+		resave: false,
+		saveUninitialized: true,
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+// below is custom middleware that makes the user variable available in all EJS templates
+// if no one is logged in, user will be undefined
+app.use(function (req, res, next) {
+	res.locals.user = req.user;
+	next();
+});
+
+// mount routes
+
+// tell app to listen for requests
+app.listen(3000, () => {
+	console.log("express is listening on port:3000");
+});
