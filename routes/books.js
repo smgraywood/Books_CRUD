a58dd27ce66d335911d44c28a8d1bb0de6d37b9e
Reese Dame <reesedame@gmail.com>
2023-04-26 19:47:50 -0700

    Books connect to user & add auth for book routes

--- routes/books.js
+++ routes/books.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const booksController = require("../controllers/books");
+const ensureLoggedIn = require("../config/ensureLoggedIn");
 
 //GET /books/new - new book route
-router.get("/new", booksController.new);
+router.get("/new", ensureLoggedIn, booksController.new);
 
 //POST /books - create route
-router.post("/", booksController.create);
+router.post("/", ensureLoggedIn, booksController.create);
 
 //GET /books - index route
-router.get("/", booksController.index);
+router.get("/", ensureLoggedIn, booksController.index);
 
 // GET /books/:id/edit - edit route
-router.get("/:id/edit", booksController.edit);
+router.get("/:id/edit", ensureLoggedIn, booksController.edit);
 
 //POST /books/:id - update route
-router.post("/:id", booksController.updateBook);
+router.post("/:id", ensureLoggedIn, booksController.updateBook);
 
 //GET /books/:id - show route, must go last
-router.get("/:id", booksController.show);
+router.get("/:id", ensureLoggedIn, booksController.show);
 
 module.exports = router;
